6390970b21f891564ed99aea997ee659c9258ac8
Greg Little <glittle@gmail.com>
2013-02-21 07:46:13 -0800

    anticipate a change in the portrailurl key in the csv

--- update_from_email.js
+++ update_from_email.js
@@ -71,12 +71,15 @@ _.run(function () {
         }
         var rows = require('./csv.js').parse('' + mo.attachments[0].content, '|', true)
         if (rows.length > 0 && !rows[0].created_ts_gmt) throw new Error('CSV PARSE ERROR!')
+
+        var portraiturlKey = _.find(_.keys(rows[0]), function (e) { return e.match(/portrait/i) })
+
         var rows = _.map(rows, function (r) {
             return {
                 time : new Date(r.created_ts_gmt).getTime(),
                 username : r.uid,
                 name : r.user_name,
-                img : r.portraiturl ? parseHyperlink(r.portraiturl) : null,
+                img : r[portraiturlKey] ? parseHyperlink(r[portraiturlKey]) : null,
                 title : r.profiletitle || null,
                 overview : r.profile_overview || null,
                 obo : parseHyperlink(r.obo_link)
